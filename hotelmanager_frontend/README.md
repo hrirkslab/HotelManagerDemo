dba76488ad2655ccb260d18c50a71d31e0bebe69
Ujwal Subedi <ujsu0002@stud.hs-kl.de>
2024-04-06 11:41:37 +0200

    fix: Resolve issue with copying 'dist' to Docker container
    feat: Rename project to 'angular_frontend'

--- hotelmanager_frontend/README.md
+++ hotelmanager_frontend/README.md
@@ -1,6 +1,6 @@
-# HotelmanagerFE
+# HotelmanagerFrontend
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.3.
 
 ## Development server
 
--- hotelmanager_frontend/src/app/app.component.spec.ts
+++ hotelmanager_frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hotelmanager-FE' title`, () => {
+  it(`should have the 'hotelmanager_frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hotelmanager-FE');
+    expect(app.title).toEqual('hotelmanager_frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hotelmanager-FE');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hotelmanager_frontend');
   });
 });
